96457093f74194fcc640caa2e6eabcf62ca49078
mistermagic12 <75017455+mistermagic12@users.noreply.github.com>
2021-01-22 20:08:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var gameState=0
+var database;
+var playerCount
+var form,player,game
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(400,400);
+  game=new Game()
+  game.getState()
+  game.start()
+  }
+
+function draw(){
+}
+
